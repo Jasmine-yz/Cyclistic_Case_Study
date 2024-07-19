aae8b676d555f3df19b4930d38bb533cf44c96aa
Jasmine <ying.js186@gmail.com>
2024-07-19 10:22:51 -0400

    Update Data Combining.sql

--- Data Combining.sql
+++ Data Combining.sql
@@ -1,1 +1,29 @@
 
+
+CREATE TABLE `project-1-429715.gda_capstone_1.2023_biketrips` AS (
+  
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_01`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_02`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_03`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_04`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_05`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_06`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_07`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_08`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_09`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_10`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_11`
+  UNION ALL
+  SELECT * FROM `project-1-429715.gda_capstone_1.2023_12`
+
+);
