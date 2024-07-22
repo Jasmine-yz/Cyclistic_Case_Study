819cada3686135fe06af04df3730a285e74661c9
Jasmine <ying.js186@gmail.com>
2024-07-22 17:25:59 -0400

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,6 +1,6 @@
 
 
-CREATE TABLE 2023_biketrips_updated AS(
+CREATE TABLE 2023_tripdata_cleaned_combined AS(
   SELECT
     ride_id,
     rideable_type,
@@ -8,27 +8,27 @@ CREATE TABLE 2023_biketrips_updated AS(
     ended_at,
     FORMAT_TIMESTAMP('%T', TIMESTAMP_SECONDS(TIMESTAMP_DIFF(ended_at,started_at,SECOND)))AS ride_length,
     CASE EXTRACT(DAYOFWEEK FROM started_at)
-      WHEN 1 THEN 'SUN'
-      WHEN 2 THEN 'MON'
-      WHEN 3 THEN 'TUES'
-      WHEN 4 THEN 'WED'
-      WHEN 5 THEN 'THURS'
-      WHEN 6 THEN 'FRI'
-      WHEN 7 THEN 'SAT'
+      WHEN 1 THEN 'Sunday'
+      WHEN 2 THEN 'Monday'
+      WHEN 3 THEN 'Tuesday'
+      WHEN 4 THEN 'Wednesday'
+      WHEN 5 THEN 'Thursday'
+      WHEN 6 THEN 'Friday'
+      WHEN 7 THEN 'Saturday'
     END AS day_of_week,
     CASE EXTRACT(MONTH FROM started_at)
-      WHEN 1 THEN 'JAN'
-      WHEN 2 THEN 'FEB'
-      WHEN 3 THEN 'MAR'
-      WHEN 4 THEN 'APR'
-      WHEN 5 THEN 'MAY'
-      WHEN 6 THEN 'JUN'
-      WHEN 7 THEN 'JUL'
-      WHEN 8 THEN 'AUG'
-      WHEN 9 THEN 'SEP'
-      WHEN 10 THEN 'OCT'
-      WHEN 11 THEN 'NOV'
-      WHEN 12 THEN 'DEC'
+      WHEN 1 THEN 'January'
+      WHEN 2 THEN 'February'
+      WHEN 3 THEN 'March'
+      WHEN 4 THEN 'April'
+      WHEN 5 THEN 'May'
+      WHEN 6 THEN 'June'
+      WHEN 7 THEN 'July'
+      WHEN 8 THEN 'August'
+      WHEN 9 THEN 'September'
+      WHEN 10 THEN 'October'
+      WHEN 11 THEN 'November'
+      WHEN 12 THEN 'December'
     END AS month,
     start_station_name,
     end_station_name,
