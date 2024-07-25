91f3c3e47d753874264f03673dc06ade5373ff80
Jasmine <ying.js186@gmail.com>
2024-07-25 17:51:31 -0400

    Update Data Combining.sql

--- Data Combining.sql
+++ Data Combining.sql
@@ -1,27 +1,27 @@
 CREATE TABLE `project-1-429715.cyclistic_tripdata.biketrips` AS (
   
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_06_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_06_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_07_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_07_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_08_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_08_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_09_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_09_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_10_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_10_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_11_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_11_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_12_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_12_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_01_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_01_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_02_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_01_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_03_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_02_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_04_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_03_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_05_biketrips`
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_04_biketrips`
 
 );
