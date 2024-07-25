f2e22bb1b95842648c636c013fbdf7e88565f7fc
Jasmine <ying.js186@gmail.com>
2024-07-25 17:30:31 -0400

    Update Data Combining.sql

--- Data Combining.sql
+++ Data Combining.sql
@@ -1,27 +1,27 @@
-CREATE TABLE `project-1-429715.divvy_tripdata.biketrips` AS (
+CREATE TABLE `project-1-429715.cyclistic_tripdata.biketrips` AS (
   
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_06_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2023_06_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_07_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2023_07_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_08_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2023_08_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_09_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2023_09_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_10_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2023_10_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_11_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2023_11_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2023_12_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2023_12_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_01_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2024_01_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_02_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2024_02_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_03_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2024_03_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_04_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2024_04_biketrips`
   UNION ALL
-  SELECT * FROM `project-1-429715.divvy_tripdata.2024_05_tripdata`
+  SELECT * FROM `project-1-429715.divvy_tripdata.2024_05_biketrips`
 
 );
