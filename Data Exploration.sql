6c36a70c153ec54576eedf03a0a2dc61a00231a0
Jasmine <ying.js186@gmail.com>
2024-07-27 12:34:38 -0400

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -4,7 +4,7 @@
 --1. Checking the number of null value in each field.
 
 SELECT
-  SUM((CASE WHEN ride_id IS NULL THEN 1 ELSE 0 END)) AS ride_id,
+  SUM(CASE WHEN ride_id IS NULL THEN 1 ELSE 0 END) AS ride_id,
   SUM(CASE WHEN rideable_type IS NULL THEN 1 ELSE 0 END) AS rideable_type,
   SUM(CASE WHEN started_at IS NULL THEN 1 ELSE 0 END) AS started_at,
   SUM(CASE WHEN ended_at IS NULL THEN 1 ELSE 0 END) AS ended_at,
@@ -18,23 +18,23 @@ SELECT
   SUM(CASE WHEN end_lng IS NULL THEN 1 ELSE 0 END) AS end_lng,
   SUM(CASE WHEN member_casual IS NULL THEN 1 ELSE 0 END) AS member_casual
   
-FROM `project-1-429715.divvy_tripdata.biketrips`
+FROM `project-1-429715.cyclistic_tripdata.2023biketrips`;
 
---2.Checking for duplicate data
+--2.Checking for duplicate data (0 duplicate)
 
 SELECT 
   ride_id,
   COUNT(*) AS num_of_duplicates
-FROM `project-1-429715.divvy_tripdata.biketrips`
-  GROUP BY ride_id
-  HAVING COUNT(ride_id) > 1;
+FROM `project-1-429715.cyclistic_tripdata.2023biketrips`
+GROUP BY ride_id 
+HAVING COUNT(*) > 1;
 
 --3.Length Constraints: 'ride_id'
 
 SELECT 
     LENGTH(ride_id) AS ride_id_length,
     COUNT(*) AS count_of_ride_id
-FROM `project-1-429715.divvy_tripdata.biketrips`
+FROM `project-1-429715.cyclistic_tripdata.2023biketrips`
 GROUP BY LENGTH(ride_id)
 ORDER BY ride_id_length;
 
@@ -43,7 +43,7 @@ ORDER BY ride_id_length;
 SELECT
   DISTINCT rideable_type,
   COUNT(*) AS num_of_trips,
-FROM `project-1-429715.divvy_tripdata.biketrips`
+FROM `project-1-429715.cyclistic_tripdata.2023biketrips`
 GROUP BY rideable_type;
 
 --5. Check the MAXUM and MINIMUM  ride_length for each rideable_type.
@@ -53,18 +53,18 @@ SELECT
   MAX(ROUND(TIMESTAMP_DIFF(ended_at, started_at, MINUTE), 2)) AS ride_length_MAX,
   MIN(ROUND(TIMESTAMP_DIFF(ended_at, started_at, MINUTE), 2)) AS ride_length_MIN
 
-FROM `project-1-429715.divvy_tripdata.biketrips`
+FROM `project-1-429715.cyclistic_tripdata.2023biketrips`
 GROUP BY rideable_type;
 
 
---The trip longer than a day (Total of rows = 7542)
+--The trip longer than a day (Total of rows = 6417)
 SELECT COUNT(*) AS longer_than_a_day
-FROM `project-1-429715.divvy_tripdata.biketrips` 
+FROM `project-1-429715.cyclistic_tripdata.2023biketrips` 
 WHERE TIMESTAMP_DIFF(ended_at,started_at,MINUTE) > 1440;
 
---The trip less than a miniute (Total of rows = 136168)
+--The trip less than a miniute (Total of rows = 149615)
 SELECT COUNT(*) AS less_than_a_mins
-FROM `project-1-429715.divvy_tripdata.biketrips` 
+FROM `project-1-429715.cyclistic_tripdata.2023biketrips` 
 WHERE TIMESTAMP_DIFF(ended_at,started_at,MINUTE) < 1;
 
 
