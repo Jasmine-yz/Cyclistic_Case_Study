362ddb50e0c721fb39764f44317b838ae6209625
Jasmine <ying.js186@gmail.com>
2024-07-27 12:00:43 -0400

    Update Data Combining.sql

--- Data Combining.sql
+++ Data Combining.sql
@@ -1,5 +1,15 @@
-CREATE TABLE `project-1-429715.cyclistic_tripdata.biketrips` AS (
+CREATE TABLE `project-1-429715.cyclistic_tripdata._2023biketrips` AS (
   
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_01_biketrips`
+  UNION ALL
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_02_biketrips`
+  UNION ALL
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_03_biketrips`
+  UNION ALL
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_04_biketrips`
+  UNION ALL
+  SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_05_biketrips`
+  UNION ALL
   SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_06_biketrips`
   UNION ALL
   SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_07_biketrips`
@@ -13,15 +23,5 @@ CREATE TABLE `project-1-429715.cyclistic_tripdata.biketrips` AS (
   SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_11_biketrips`
   UNION ALL
   SELECT * FROM `project-1-429715.cyclistic_tripdata.2023_12_biketrips`
-  UNION ALL
-  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_01_biketrips`
-  UNION ALL
-  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_01_biketrips`
-  UNION ALL
-  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_02_biketrips`
-  UNION ALL
-  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_03_biketrips`
-  UNION ALL
-  SELECT * FROM `project-1-429715.cyclistic_tripdata.2024_04_biketrips`
-
+  
 );
